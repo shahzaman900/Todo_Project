f8cb69bcbe0b83395ce9c64fd7254e599025e517
sheezu900 <shahzamanlive2@gmail.com>
2023-07-12 18:53:18 +0500

    setup linter and setup webpack

--- src/dist/index.dev.js
+++ src/dist/index.dev.js
@@ -1,1 +1,29 @@
 "use strict";
+
+var _lodash = _interopRequireDefault(require("lodash"));
+
+require("./style.css");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+// eslint-disable-next-line no-unused-vars
+function component() {}
+
+document.body.appendChild(component());
+var tasks = [{
+  discription: 'tasks',
+  complete: true,
+  index: 1
+}, {
+  discription: 'tasks',
+  complete: true,
+  index: 2
+}, {
+  discription: 'tasks',
+  complete: true,
+  index: 3
+}, {
+  discription: 'tasks',
+  complete: true,
+  index: 3
+}];
---
+++ src/index.js
@@ -0,0 +1,30 @@
+// eslint-disable-next-line no-unused-vars
+import _ from 'lodash';
+import './style.css';
+
+function component() {
+}
+document.body.appendChild(component());
+
+const tasks = [
+  {
+    discription: 'tasks',
+    complete: true,
+    index: 1,
+  },
+  {
+    discription: 'tasks',
+    complete: true,
+    index: 2,
+  },
+  {
+    discription: 'tasks',
+    complete: true,
+    index: 3,
+  },
+  {
+    discription: 'tasks',
+    complete: true,
+    index: 3,
+  },
+];
