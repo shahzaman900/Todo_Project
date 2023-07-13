db130b8c37a920acaa3f17c1e9b02f6fd436e08d
sheezu900 <shahzamanlive2@gmail.com>
2023-07-13 15:22:18 +0500

    Create list dynamically and display it

--- src/dist/index.dev.js
+++ src/dist/index.dev.js
@@ -1,29 +1,52 @@
 "use strict";
 
-var _lodash = _interopRequireDefault(require("lodash"));
-
 require("./style.css");
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-// eslint-disable-next-line no-unused-vars
-function component() {}
-
-document.body.appendChild(component());
 var tasks = [{
-  discription: 'tasks',
+  description: 'task 1',
   complete: true,
   index: 1
 }, {
-  discription: 'tasks',
+  description: 'task 2',
   complete: true,
   index: 2
 }, {
-  discription: 'tasks',
+  description: 'task 3',
   complete: true,
-  index: 3
+  index: 4
 }, {
-  discription: 'tasks',
+  description: 'task 4',
   complete: true,
   index: 3
 }];
+document.addEventListener('DOMContentLoaded', function () {
+  var taskList = document.getElementById('todo_list_section');
+
+  function displayTaskList() {
+    // Sort tasks based on index value
+    tasks.sort(function (a, b) {
+      return a.index - b.index;
+    });
+    tasks.forEach(function (task) {
+      var list = document.createElement('ul');
+      list.classList.add('list');
+      var listItem = document.createElement('li');
+      listItem.classList.add('list_item');
+      var checkBox = document.createElement('span');
+      checkBox.classList.add('material-symbols-outlined');
+      checkBox.textContent = 'check_box_outline_blank';
+      var description = document.createElement('p');
+      description.textContent = task.description;
+      var moreVert = document.createElement('span');
+      moreVert.classList.add('material-symbols-outlined');
+      moreVert.textContent = 'more_vert';
+      listItem.appendChild(checkBox);
+      listItem.appendChild(description);
+      listItem.appendChild(moreVert);
+      list.appendChild(listItem);
+      taskList.appendChild(list);
+    });
+  }
+
+  displayTaskList();
+});
--- src/index.js
+++ src/index.js
@@ -1,30 +1,62 @@
-// eslint-disable-next-line no-unused-vars
-import _ from 'lodash';
 import './style.css';
-
-function component() {
-}
-document.body.appendChild(component());
 
 const tasks = [
   {
-    discription: 'tasks',
+    description: 'task 1',
     complete: true,
     index: 1,
   },
   {
-    discription: 'tasks',
+    description: 'task 2',
     complete: true,
     index: 2,
   },
   {
-    discription: 'tasks',
+    description: 'task 3',
     complete: true,
-    index: 3,
+    index: 4,
   },
   {
-    discription: 'tasks',
+    description: 'task 4',
     complete: true,
     index: 3,
   },
 ];
+
+document.addEventListener('DOMContentLoaded', () => {
+  const taskList = document.getElementById('todo_list_section');
+
+  function displayTaskList() {
+    // Sort tasks based on index value
+    tasks.sort((a, b) => a.index - b.index);
+
+    tasks.forEach((task) => {
+      const list = document.createElement('ul');
+      list.classList.add('list');
+
+      const listItem = document.createElement('li');
+      listItem.classList.add('list_item');
+
+      const checkBox = document.createElement('span');
+      checkBox.classList.add('material-symbols-outlined');
+      checkBox.textContent = 'check_box_outline_blank';
+
+      const description = document.createElement('p');
+      description.textContent = task.description;
+
+      const moreVert = document.createElement('span');
+      moreVert.classList.add('material-symbols-outlined');
+      moreVert.textContent = 'more_vert';
+
+      listItem.appendChild(checkBox);
+      listItem.appendChild(description);
+      listItem.appendChild(moreVert);
+
+      list.appendChild(listItem);
+
+      taskList.appendChild(list);
+    });
+  }
+
+  displayTaskList();
+});
